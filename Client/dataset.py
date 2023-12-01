4913feca9d2c06e42a02d40828b14a8bb6718a1f
steve <you@example.com>
2023-12-01 17:00:52 +0000

    add more implementation deets

--- Client/dataset.py
+++ Client/dataset.py
@@ -1,21 +1,40 @@
 
 import pytorch_lightning as pl
-from torchvision.datasets import CocoDetection
+from torchvision.datasets import CocoCaptions
 from torch.utils.data import DataLoader
+import torch
+from clip.simple_tokenizer import SimpleTokenizer
+
+class TokenizedCocoCaptions(CocoCaptions):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.tokenizer = SimpleTokenizer()
+    def __getitem__(self, index):
+        img, target = super().__getitem__(index)
+        tokenized_target = self.tokenizer.tokenize(target)
+        return img, tokenized_target
+
 
 class CocoDataModule(pl.LightningDataModule):
     def __init__(self, data_dir, batch_size=32):
         super().__init__()
         self.data_dir = data_dir
         self.batch_size = batch_size
+        #use tokenizers to convert the captions into a format that can be used by the CLIP model
 
     def prepare_data(self):
-        # Download or preprocess the COCO dataset if needed
-        CocoDetection(root=self.data_dir, annFile='annotations.json', transform=None)
+        #check that the data is available
+        TokenizedCocoCaptions(root=self.data_dir, annFile='annotations.json', transform=None)
+
 
     def setup(self, stage=None):
         # Load the COCO dataset
-        self.dataset = CocoDetection(root=self.data_dir, annFile='annotations.json', transform=None)
+        # to properly test this federated learning example, we would need to split the dataset into a subset for each client
+        # for simplicity, we will just use a random 50% of the dataset for each client
+        dataset=TokenizedCocoCaptions(root=self.data_dir, annFile='annotations.json', transform=None)
+
+        self.dataset,_ =  torch.utils.data.random_split(dataset, [len(dataset)//2, len(dataset)-len(dataset)//2])
+        
 
     def train_dataloader(self):
         # Create a DataLoader for training data
--- Client/train.py
+++ Client/train.py
@@ -2,15 +2,33 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import pytorch_lightning as pl
-
+import random
 class PermutationLayer(nn.Module):
-    def __init__(self):
+    def __init__(self,key):
+
         super(PermutationLayer, self).__init__()
         # Define your permutation layer logic here
-        
+        self.key = key
+        #the key is a list of 0s and 1s that is used to determine the permutation,
+        #so the permutation is the same for each key
+        #for example, if the key is '101', then the permutation is [0,2,1]
+        #if the key is '010', then the permutation is [1,0,2]
+        #make out seed by converting the key to an integer as a binary number
+        asbool= [bool(int(i)) for i in key]
+        #convert the list of booleans to a binary number
+        asint= int(''.join([str(int(i)) for i in asbool]),2)
+
+        #we use a random generator to generate the permutation but seed it with the number of trues in the key
+        self.generator = torch.Generator().manual_seed(asint)
+        # let n be the number of trues in the key
+        n= sum(key)
+        perm= torch.randperm(n,generator=self.generator)
+        self.permutations = torch.arange(len(self.key))
+        self.permutations= torch.where(key, self.permutations[self.key][perm],self.permutations)
+
     def forward(self, x):
         # Implement the forward pass of your permutation layer here
-        pass
+        return x[self.permutations]
 
 class CLIPModel(pl.LightningModule):
     def __init__(self):
@@ -30,13 +48,23 @@ class CLIPModel(pl.LightningModule):
         # Implement the validation step logic here
         # Send the model weights to the server and receive deltas
         weights = self.state_dict()
-        deltas = send_weights_to_server(weights)
+        deltas = self.send_weights_to_server(weights)
         return deltas
     
     def configure_optimizers(self):
         optimizer = optim.AdamW(self.parameters())
         return optimizer
 
+
+if __name__ == '__main__':
+    import sys
+    #get key for model from first CLI argument
+    key = sys.argv[1]
+    from dataset import *    
+    data_module = CocoDataModule(data_dir='./data', batch_size=32)
+    model = CLIPModel(key)
+    trainer = pl.Trainer()
+    trainer.fit(model)
 model = CLIPModel()
 
 # Load the weights from the server
